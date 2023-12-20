b9d93d48e4d7f1b49f227a964b51b354ee419642
Luke Manary <69057730+xvyrq@users.noreply.github.com>
2023-12-20 17:37:26 -0600

    updated nav css

--- src/Nav.js
+++ src/Nav.js
@@ -1,18 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import './Nav.css';
 
 function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return (
         <nav className="navbar">
-        <div className="navbar-brand"><img className="endlessLogoNav" src="/endlessdevLogo.webp" alt="Endless Dev Logo"/>Endless Design</div>
-        <ul className="navbar-nav">
-          <li className="nav-item"><a href="#home">Home</a></li>
-          <li className="nav-item"><a href="#about">About</a></li>
-          <li className="nav-item"><a href="#projects">Projects</a></li>
-          <li className="nav-item"><a href="#contact">Contact</a></li>
-        </ul>
-      </nav>
+            <div className="navbar-brand">
+                <img className="endlessLogoNav" src="/endlessdevLogo.webp" alt="Endless Dev Logo"/>
+                Endless Design
+            </div>
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <div className="line"></div>
+                <div className="line"></div>
+                <div className="line"></div>
+            </div>
+            <ul className={`navbar-nav ${isOpen ? "open" : ""}`}>
+                <li className="nav-item"><a href="#home">Home</a></li>
+                <li className="nav-item"><a href="#about">About</a></li>
+                <li className="nav-item"><a href="#projects">Projects</a></li>
+                <li className="nav-item"><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
     );
 }
 
-export default Nav
+export default Nav;
